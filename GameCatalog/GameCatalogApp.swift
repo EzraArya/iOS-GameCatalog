0afb139dcc9894430f802605e4b20252a78d6055
EzraArya <ezrawijaya10@gmail.com>
2024-08-27 00:33:27 +0700

    Initial Commit

---
+++ GameCatalog/GameCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCatalogApp.swift
+//  GameCatalog
+//
+//  Created by Ezra Arya Wijaya on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GameCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
